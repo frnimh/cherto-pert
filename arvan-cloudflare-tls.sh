3b0d36651a7a608bed7367a48ca80a9be90b88ad
frnimh <erfanghavam@live.com>
2023-01-22 21:26:49 +0330

    fix error output issue

--- arvan-cloudflare-tls.sh
+++ arvan-cloudflare-tls.sh
@@ -4,11 +4,12 @@
 
 ARED='\x1b[31;1m'
 AURL='\x1b[39;49m'
+ANO='\033[0m'
 ACME="$HOME/.acme.sh/"
 
 if ! [ ${ACME}/acme.sh 2>&1 1>/dev/null ] && [ acme.sh 2>&1 1>/dev/null ]; then
         echo -e "${ARED}acme not installed or aliases not set"
-	echo -e "checkout ${AURL}https://github.com/acmesh-official/acme.sh/wiki/How-to-install"
+	echo -e "checkout ${AURL}https://github.com/acmesh-official/acme.sh/wiki/How-to-install${ANO}"
         exit 1
 fi
 
